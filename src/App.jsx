4fc60a61922aaa59cdad95ff35b4398a3c1cba59
villanueva <villanueva.guerrero@outlook.com>
2024-12-23 00:22:42 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -67,9 +67,12 @@ function App() {
           </div>
         </section>
         <section className='joinus_section'>
-          <h2>Únete a Nosotros</h2>
-          <p>¡Conviértete en Distribuidor!
-          Forma parte de nuestra red de distribuidores y lleva la fuerza y pureza de El Corsario a más personas.</p>
+          <div className='joinus_section__content'>
+            <h2>Únete a Nosotros</h2>
+            <p>¡Conviértete en Distribuidor!
+            Forma parte de nuestra red de distribuidores y lleva la fuerza y pureza de El Corsario a más personas.</p>
+          </div>
+          <button>LLenar formulario</button>
         </section>
       </main>
       <footer>
